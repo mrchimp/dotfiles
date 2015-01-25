a7f7dcd92ad6e90b5b5d38c347684dfee40872ab
mrchimp <chimpytk@gmail.com>
2015-01-25 23:42:04 +0000

    Fix copy command.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 dest=${1:-~}
 
 echo "Copying to $dest"
-
-cp -rv ./files/* ./files/.* $dest
+ 
+cp -rv ./files/.bash_prompt $dest
+cp -rv ./files/.bashrc $dest
+cp -rv ./files/_vimrc $dest
+cp -rv ./files/.vim $dest
 
