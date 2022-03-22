d8062b12e653239f3a6eb086d9570e6eb4c9d5e1
Jake Gully <chimpytk@gmail.com>
2022-03-22 20:45:30 +0000

    Improve install script

--- setup.sh
+++ setup.sh
@@ -2,58 +2,77 @@
 
 DEST=${1:-~}
 
-echo "Installing software. You may be asked for your password"
-echo " - vim, zsh, ack"
-sudo apt-get install -yq vim zsh ack
+echo "Installing software"
+echo -n "→ vim, zsh, ack ... "
+sudo apt-get -yqq install vim zsh ack
+echo -e "\e[32mok\e[0m"
 
-echo " - oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -n "→ oh-my-zsh ... "
 
-echo "Copying to $DEST"
-
-echo "  → .ackrc"
-cp -r ./files/.ackrc $DEST
-
-echo "  → .bash_prompt"
-cp -r ./files/.bash_prompt $DEST
-
-echo "  → .bashrc"
-cp -r ./files/.bashrc $DEST
-
-echo "  → .zshrc"
-cp -r ./files/.zshrc $DEST
-
-echo "  → .gitconfig"
-cp -r ./files/.gitconfig $DEST
-
-echo "  → _vimrc"
-cp -r ./files/_vimrc $DEST
-
-echo "  → .vim"
-cp -r ./files/.vim $DEST
-
-echo "  → .tmux.conf"
-cp -r ./files/.tmux.conf $DEST
-
-echo "  → .zshrc"
-cp -r ./files/.zshrc $DEST
-
-echo "  → scripts"
-cp -r ./files/scripts $DEST
-
-echo "  → bash"
-cp -r ./files/bash $DEST
-
-echo "  → cheatsheets"
-cp -r ./files/cheatsheets $DEST
-
-if [ ! -f $DEST/aliases.sh ]; then
-  echo "  → aliases"
-  cp ./files/aliases.sh $DEST/aliases.sh
+if [ -z $ZSH ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo -e "\e[32mok\e[0m"
+else
+  echo -e "\e[33mSkipped. Already installed.\e[0m"
 fi
 
-
-echo "Installing bash-powerline - https://github.com/riobard/bash-powerline"
+echo -n "→ bash-powerline ... "
 curl -s https://raw.githubusercontent.com/riobard/bash-powerline/master/bash-powerline.sh > ~/.bash-powerline.sh
+echo -e "\e[32mok\e[0m"
 
-echo -e "\033[32mAll done!\033[0m"
+echo ""
+echo "Copying files to $DEST"
+
+echo -n "→ .ackrc ... "
+cp -r ./files/.ackrc $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .bash_prompt ... "
+cp -r ./files/.bash_prompt $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .bashrc ... "
+cp -r ./files/.bashrc $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .zshrc ... "
+cp -r ./files/.zshrc $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .gitconfig ... "
+cp -r ./files/.gitconfig $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ _vimrc ... "
+cp -r ./files/_vimrc $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .vim ... "
+cp -r ./files/.vim $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .tmux.conf ... "
+cp -r ./files/.tmux.conf $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ .zshrc ... "
+cp -r ./files/.zshrc $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ scripts ... "
+cp -r ./files/scripts $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ bash ... "
+cp -r ./files/bash $DEST
+echo -e "\e[32mok\e[0m"
+
+echo -n "→ cheatsheets ... "
+cp -r ./files/cheatsheets $DEST
+echo -e "\e[32mok\e[0m"
+
+if [ ! -f $DEST/aliases.sh ]; then
+  echo -n "→ aliases ... "
+  cp ./files/aliases.sh $DEST/aliases.sh
+  echo -e "\e[32mok\e[0m"
+fi
